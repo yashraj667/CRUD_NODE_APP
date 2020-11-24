dc66129efd7e6c2a4d9f36aae92849d749e25ccd
yashraj667 <48904031+yashraj667@users.noreply.github.com>
2020-11-24 12:37:35 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const dbConn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'yash@123',
+  password : '********',
   database : 'mydb'
 });
 dbConn.connect(function(err) {
